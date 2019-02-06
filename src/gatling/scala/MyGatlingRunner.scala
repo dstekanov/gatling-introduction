b813186a0ab399514dfb3c845e7e05976167c321
Dmytro_Stekanov <dstekanov@gmail.com>
2019-02-06 17:15:19 +0200

    Code reuse with objects

--- src/gatling/scala/MyGatlingRunner.scala
+++ src/gatling/scala/MyGatlingRunner.scala
@@ -1,12 +1,12 @@
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import simulations.CheckResponseBodyAndExtract
+import simulations.CodeReuseWithObjects
 
 object MyGatlingRunner {
 
   def main(args: Array[String]): Unit = {
 
-    val simClass = classOf[CheckResponseBodyAndExtract].getName
+    val simClass = classOf[CodeReuseWithObjects].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
---
+++ src/gatling/scala/simulations/CodeReuseWithObjects.scala
@@ -0,0 +1,32 @@
+package simulations
+
+import config.BaseSimulation
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+class CodeReuseWithObjects extends BaseSimulation {
+
+  val scn = scenario("Video Game DB")
+    .exec(getAllVideoGames)
+    .pause(5)
+    .exec(getSpecificVideoGame(1))
+    .pause(5)
+    .exec(getAllVideoGames)
+
+  def getAllVideoGames: ChainBuilder = {
+    exec(http("Get All Video Games")
+      .get("videogames")
+      .check(status.is(200)))
+  }
+
+  def getSpecificVideoGame(gameId: Long): ChainBuilder = {
+    exec(http("Get Specific Game")
+      .get(s"videogames/$gameId")
+      .check(status.in(200 to 201)))
+  }
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
