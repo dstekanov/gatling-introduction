3d86f3b2cbf491d71431e96aa802b7dc66f368f5
Dmytro_Stekanov <dstekanov@gmail.com>
2019-02-07 16:19:06 +0200

    Update gatling plugin version to 3.0.2

--- src/gatling/scala/config/BaseSimulation.scala
+++ src/gatling/scala/config/BaseSimulation.scala
@@ -2,13 +2,12 @@ package config
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.http.protocol.HttpProtocolBuilder
 
 class BaseSimulation extends Simulation {
 
   // 1 Common HTTP Configuration
-  val httpConf: HttpProtocolBuilder = http
-    .baseURL("http://localhost:8080/app/")
+  val httpConf = http
+    .baseUrl("http://localhost:8080/app/")
     .header("Accept", "application/json")
   //    .proxy(Proxy("localhost", 8888).httpsPort(8888))
 
