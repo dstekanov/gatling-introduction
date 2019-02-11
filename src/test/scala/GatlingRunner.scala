c2e15c4d4b479f177ee61404d41b300e1b2018fd
Dmytro_Stekanov <dstekanov@gmail.com>
2019-02-11 13:47:24 +0200

    move to Maven Plugin

--- src/test/scala/GatlingRunner.scala
+++ src/test/scala/GatlingRunner.scala
@@ -1,12 +1,13 @@
+
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import simulations.FixedDurationLoadSimulation
+import simulations.AddPauseTime
 
 object GatlingRunner {
 
   def main(args: Array[String]): Unit = {
 
-    val simClass = classOf[FixedDurationLoadSimulation].getName
+    val simClass = classOf[AddPauseTime].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
---
+++ src/test/scala/simulations/RuntimeParameters.scala
@@ -0,0 +1,29 @@
+package simulations
+
+import config.BaseSimulation
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+class RuntimeParameters extends BaseSimulation {
+
+  def getAllVideoGames: ChainBuilder = {
+    exec(http("Get All Video Games")
+      .get("videogames")
+      .check(status.is(200)))
+  }
+
+  val scn = scenario("Video Game DB")
+    .forever() {
+      exec(getAllVideoGames)
+    }
+
+  setUp(
+    scn.inject(
+      nothingFor(5 seconds),
+      rampUsers(1) during (1 second)
+    )
+  ).protocols(httpConf).maxDuration(20 seconds)
+}
