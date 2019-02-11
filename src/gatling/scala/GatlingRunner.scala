918fffd425efd7d23f19c59350711466a7a88a00
Dmytro_Stekanov <dstekanov@gmail.com>
2019-02-11 10:53:15 +0200

    Add test RampUsersLoadSimulation

--- src/gatling/scala/GatlingRunner.scala
+++ src/gatling/scala/GatlingRunner.scala
@@ -1,12 +1,11 @@
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import simulations.BasicLoadSimulation
 
 object GatlingRunner {
 
   def main(args: Array[String]): Unit = {
 
-    val simClass = classOf[BasicLoadSimulation].getName
+    val simClass = classOf[RampUsersLoadSimulation].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
---
+++ src/gatling/scala/simulations/RampUsersLoadSimulation.scala
@@ -0,0 +1,34 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+class RampUsersLoadSimulation extends BaseSimulation {
+
+  val scn = scenario("Video Game DB")
+    .exec(getAllVideoGames)
+    .pause(5)
+    .exec(getSpecificVideoGame(2))
+    .pause(5)
+    .exec(getAllVideoGames)
+
+  def getAllVideoGames: ChainBuilder = {
+    exec(http("Get All Video Games")
+      .get("videogames")
+      .check(status.is(200)))
+  }
+
+  def getSpecificVideoGame(gameId: Long): ChainBuilder = {
+    exec(http("Get Specific Game")
+      .get(s"videogames/$gameId")
+      .check(status.in(200 to 201)))
+  }
+
+  setUp(
+    scn.inject(
+      nothingFor(5),
+      rampUsersPerSec(1) to 5 during 20
+    )
+  ).protocols(httpConf.inferHtmlResources())
+}
