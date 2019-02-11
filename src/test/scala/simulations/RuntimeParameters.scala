ae0887dfb77955bf0ba0ce102ca9b059f6053b00
Dmytro_Stekanov <dstekanov@gmail.com>
2019-02-11 14:04:22 +0200

    Runtime Parameters

--- src/test/scala/simulations/RuntimeParameters.scala
+++ src/test/scala/simulations/RuntimeParameters.scala
@@ -9,6 +9,22 @@ import scala.concurrent.duration.DurationInt
 
 class RuntimeParameters extends BaseSimulation {
 
+  private def getProperty(propertyName: String, defaultValue: String) = {
+    Option(System.getenv(propertyName))
+      .orElse(Option(System.getProperty(propertyName)))
+      .getOrElse(defaultValue)
+  }
+
+  private val userCount: Int = getProperty("USERS", "5").toInt
+  private val rampDuration: Int = getProperty("RAMP_DURATION", "10").toInt
+  private val testDuration: Int = getProperty("DURATION", "60").toInt
+
+  before {
+    println(s"Running test with $userCount users")
+    println(s"Rumping users over $rampDuration seconds")
+    println(s"Total test duration $testDuration seconds")
+  }
+
   def getAllVideoGames: ChainBuilder = {
     exec(http("Get All Video Games")
       .get("videogames")
@@ -23,7 +39,7 @@ class RuntimeParameters extends BaseSimulation {
   setUp(
     scn.inject(
       nothingFor(5 seconds),
-      rampUsers(1) during (1 second)
+      rampUsers(userCount) during (rampDuration second)
     )
-  ).protocols(httpConf).maxDuration(20 seconds)
+  ).protocols(httpConf).maxDuration(testDuration seconds)
 }
